de2f618a747919b1155cc09dc942b6692342efb7
twenty0ne <twenty0ne@qq.com>
2017-09-01 00:45:18 +0800

    update

--- python/2kmtcentral/main.py
+++ python/2kmtcentral/main.py
@@ -4,6 +4,16 @@ from bs4 import BeautifulSoup
 from bs4 import element
 
 playerUrls = []
+attrKeys = []
+
+def updateAttrKey(key):
+    if attrKeys.count(key) == 0:
+        attrKeys.append(key)
+
+def filterAttrVal(val):
+    for k in ['def.', 'Def.', 'attr.', 'Off.']:
+        val = val.replace(k, '')
+    return val
 
 def parsePlayerUrl(url):
     try:
@@ -14,32 +24,46 @@ def parsePlayerUrl(url):
 
         soup = BeautifulSoup(html)
 
+        tagPlayerView = soup.find('div', id='player-view')
+        tagPlayerInfos = tagPlayerView.find('div', class_='col-xs-12 col-md-8')
+        name = tagPlayerInfos.div.header.h1.text
+        tagInfo = tagPlayerInfos.find('section', class_='infomation')
+
         tagAttrContainer = soup.find('div', class_='attributes-container')
         for tagDiv in tagAttrContainer.find_all('div'):
-            for tagSection in tagDiv.find_all('section', class_='defending'):
+            # for tagSection in tagDiv.find_all('section', class_='total'):
+            for tagSection in tagDiv.find_all('section'):
                 # parse attr header
                 tagHeader = tagSection.find('h4', class_='attribute-header')
-                if tagHeader:
-                    key = tagHeader.strong.get_text()
-                    val = tagHeader.find
+                if tagHeader and len(tagHeader.contents) > 0:
+                    #key = tagHeader.strong.get_text()
+                    #val = tagHeader.find('span', class_='attribute-box').get_text()
+                    #infos[key] = val
+                    key, val = tagHeader.text.split(' ', 1)
+                    val = filterAttrVal(val)
                     infos[key] = val
+                    updateAttrKey(key)
 
                 # parse attr list
                 tagList = tagSection.find('ul', class_='attribute-list')
                 if tagList:
                     for tagLi in tagList.find_all('li', class_='attribute'):
-                        key = ""
-                        val = 0
-                        for cot in tagLi.contents:
-                            if type(cot) == type(element.NavigableString):
-                                key = key + cot
-                            #elif type(cot) == type(BeautifulSoup.element.Tag):
-                            else:
-                                if cot['class'][0] == 'attribute-box':
-                                    val = cot.get_text()
-                                else:
-                                    key = key + cot.get_text()
-        pass
+                        # key = ""
+                        # val = 0
+                        # for cot in tagLi.contents:
+                        #     if isinstance(cot, element.NavigableString):
+                        #         key = key + cot
+                        #     #elif type(cot) == type(BeautifulSoup.element.Tag):
+                        #     else:
+                        #         if cot['class'][0] == 'attribute-box':
+                        #             val = cot.get_text()
+                        #         else:
+                        #             key = key + cot.get_text()
+                        key, val = tagLi.text.split(' ', 1)
+                        val = filterAttrVal(val)
+                        infos[key] = val
+                        updateAttrKey(key)
+
 
 
     except urllib2.HTTPError, e:
