84fba726878c6f21071301aafad2ff92e5cf636c
twenty0ne <twenty0ne@qq.com>
2017-09-01 17:32:19 +0800

    update

--- python/2kmtcentral/main.py
+++ python/2kmtcentral/main.py
@@ -1,47 +1,106 @@
+# -*- coding:utf-8 -*-
+
 import sys
 import urllib, urllib2
 from bs4 import BeautifulSoup
 from bs4 import element
 import re
+import xlsxwriter
+import time
+import datetime
+import urllib
 
 allPlayerData = {}
-playerUrls = []
-attrKeys = []
+allPlayerUrls = []
 
-def updateAttrKey(key):
-    if attrKeys.count(key) == 0:
-        attrKeys.append(key)
+infoKeys = ['Name','Theme','Position','Team','Height','Weight','Age','From']
+attrKeys = []
+rankKeys = []
+tendKeys = []
+
+totalPlayerUrlNum = 0
+
+def updateKeys(group, key):
+    if group.count(key) == 0:
+        group.append(key)
 
 def filterAttrKey(val):
     for k in ['def.', 'Def.', 'attr.', 'Off.']:
         val = val.replace(k, '')
-    return val
+    tmp = val.find(' +')
+    if tmp != -1:
+        val = val[0:tmp]
+    return val.strip()
 
 def filterInfoVal(val):
     #return val.replace('\\', '')
     return val
 
+def saveToExcel():
+    try:
+        outfile = 'output_' + datetime.datetime.now().strftime("%Y%m%d_%H%M") + '.xlsx'
+
+        wbk = xlsxwriter.Workbook(outfile)
+        sheet = wbk.add_worksheet("sheet 1")
+        nrow = 0
+        ncol = 0
+        colKeys = ['Id', 'Url']
+        #
+        for kk in infoKeys:
+            colKeys.append(kk)
+        for kk in rankKeys:
+            colKeys.append(kk)
+        for kk in attrKeys:
+            colKeys.append(kk)
+        for kk in tendKeys:
+            colKeys.append(kk)
+        for kk in colKeys:
+            sheet.write(nrow, ncol, kk)
+            ncol = ncol + 1
+
+        #
+        idx = 0
+        for url in allPlayerUrls:
+            print 'x-' + "%.2f" % ((idx + 1) * 1.0 / totalPlayerUrlNum * 100) + '% save url: ' + url
+            nrow = nrow + 1
+            idx = idx + 1
+            ncol = 0
+            datPlayer = allPlayerData[url]
+            for kk in colKeys:
+                if kk == 'Id':
+                    sheet.write(nrow, ncol, str(idx))
+                elif kk == 'Url':
+                    sheet.write(nrow, ncol, url.encode('utf-8'))
+                elif kk in datPlayer['info'].keys():
+                    sheet.write(nrow, ncol, datPlayer['info'][kk])
+                elif kk in datPlayer['attr'].keys():
+                    sheet.write(nrow, ncol, datPlayer['attr'][kk])
+                elif kk in datPlayer['rank'].keys():
+                    sheet.write(nrow, ncol, datPlayer['rank'][kk])
+                elif kk in datPlayer['tend'].keys():
+                    sheet.write(nrow, ncol, datPlayer['tend'][kk])
+                # else:
+                #     sheet.write(nrow, ncol, 'N/A')
+                ncol = ncol + 1
+
+        #
+
+        wbk.close()
+    except:
+        print 'unknow error'
+
 def parsePlayerUrl(url):
     try:
-        print "x- parse url: " + str(url)
         datPlayer = {}
 
-        rsp = urllib2.urlopen(url)
+        nurl = urllib.quote(url.encode('utf-8'), ':/')
+        rsp = urllib2.urlopen(nurl)
         html = rsp.read()
 
         soup = BeautifulSoup(html)
 
         # infos
-        infos = {
-            'Name':'',
-            'Theme':'',
-            'Position':'',
-            'Team':'',
-            'Height':'',
-            'Weight':'',
-            'Age':'',
-            'From':''
-        }
+        infos = {}
         datPlayer['info'] = infos
 
         tagPlayerView = soup.find('div', id='player-view')
@@ -51,7 +110,7 @@ def parsePlayerUrl(url):
 
         tagInfos = tagPlayerInfos.find('section', class_='information')
         for tagInfo in tagInfos.find_all('div', class_=re.compile('^col-xs-')):
-            if tagInfo.h6 and str(tagInfo.h6.text) in infos.keys():
+            if tagInfo.h6 and str(tagInfo.h6.text) in infoKeys:
                 infoKey = str(tagInfo.h6.text)
                 infos[infoKey] = filterInfoVal(tagInfo.span.text)
 
@@ -72,7 +131,7 @@ def parsePlayerUrl(url):
                     val, key = tagHeader.text.split(' ', 1)
                     key = filterAttrKey(key)
                     attrs[key] = val
-                    updateAttrKey(key)
+                    updateKeys(attrKeys, key)
 
                 # parse attr list
                 tagList = tagSection.find('ul', class_='attribute-list')
@@ -92,7 +151,7 @@ def parsePlayerUrl(url):
                         val, key = tagLi.text.split(' ', 1)
                         key = filterAttrKey(key)
                         attrs[key] = val
-                        updateAttrKey(key)
+                        updateKeys(attrKeys, key)
 
         # ranking
         ranks = {}
@@ -103,45 +162,99 @@ def parsePlayerUrl(url):
             key = tagRank.span.text
             val = str(tagRank.contents[0])
             ranks[key] = val
+            updateKeys(rankKeys, key)
 
-        #
+        # tendencies
+        tends = {}
+        datPlayer['tend'] = tends
+
+        rsp = urllib2.urlopen(nurl + "/tendencies")
+        html = rsp.read()
+        soup = BeautifulSoup(html)
+
+        tagAttrContainer = soup.find('div', class_='attributes-container')
+        for tagSection in tagAttrContainer.find_all('section'):
+            # parse attr header
+            # tagHeader = tagSection.find('h4', class_='attribute-header')
+            # if tagHeader and len(tagHeader.contents) > 0:
+            #     val, key = tagHeader.text.split(' ', 1)
+            #     key = filterAttrKey(key)
+            #     attrs[key] = val
+            #     updateAttrKey(key)
+
+            # parse attr list
+            tagList = tagSection.find('ul', class_='attribute-list')
+            if tagList:
+                for tagLi in tagList.find_all('li', class_='attribute'):
+                    val, key = tagLi.text.split(' ', 1)
+                    key = filterAttrKey(key)
+                    tends[key] = val
+                    updateKeys(tendKeys, key)
 
         #
         allPlayerData[url] = datPlayer
 
     except urllib2.HTTPError, e:
-        print 'http err: ' + e.code
+        print 'http error: ' + e.code
     except urllib2.URLError, e:
-        print 'url err: ' + e.args
+        print 'url error: ' + e.args
     except:
-        print 'unknow err'
+        print 'unknow error'
 
-def main():
+def parseAllUrl(fromIndex, toIndex):
     try:
-        idxPage = 0
+        idxPage = fromIndex
 
-        while idxPage < 1:
+        while idxPage < toIndex:
+            print "x- parse page: " + "http://2kmtcentral.com/17/players/page/" + str(idxPage)
             rsp = urllib2.urlopen("http://2kmtcentral.com/17/players/page/" + str(idxPage))
             html = rsp.read()
 
             soup = BeautifulSoup(html)
             tags = soup.find_all('tr', class_="box-click")
 
+            if tags == None or len(tags) == 0:
+                break
+
             for tag in tags:
                 url = tag.a['href']
-                playerUrls.append(url)
+                allPlayerUrls.append(url)
 
             #
             idxPage = idxPage + 1
 
-        for url in playerUrls:
-            parsePlayerUrl(url)
-
     except urllib2.HTTPError, e:
-        print (e.code)
+        print 'http error: ' + e.code
     except urllib2.URLError, e:
-        print (e.args)
+        print 'url error: ' + e.args
+    except:
+        print 'unknow error'
 
 if __name__ == '__main__':
-    #main()
-    parsePlayerUrl("http://2kmtcentral.com/17/players/8015/magic-johnson")
+    try:
+        beginTime = time.time()
+        print "-------------start parse all pages"
+        parseAllUrl(10, 15)
+
+        # test
+        # u'http://2kmtcentral.com/17/players/20544/nenê'
+        url = u'http://2kmtcentral.com/17/players/20544/nenê'
+        allPlayerUrls = []
+        allPlayerUrls.append(url)
+
+        print "-------------start parse player url"
+        totalPlayerUrlNum = len(allPlayerUrls)
+        for i in range(0, totalPlayerUrlNum):
+            print "x-" + "%.2f"%((i+1)*1.0/totalPlayerUrlNum*100) + "% parse url: " + allPlayerUrls[i]
+            parsePlayerUrl(allPlayerUrls[i])
+        print "-------------start save to excel"
+        #allPlayerUrls.append("http://2kmtcentral.com/17/players/8015/magic-johnson")
+        #parsePlayerUrl("http://2kmtcentral.com/17/players/8015/magic-johnson")
+        saveToExcel()
+
+        #
+        endTime = time.time()
+        print "Total time: " + str(endTime - beginTime)
+
+    except:
+        print "unknow error"
